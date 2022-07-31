9fd11c2c53dda6c9742dd702c33c50090972f933
SelmiAbderrahim <codingwithselmi@gmail.com>
2022-07-31 20:15:56 +0100

    initial

--- run/follow.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-SHELL=/bin/bash
-DISPLAY=:0
-PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-cd /home/selmi/socialMediaAutomation/lnpybot
-echo '' > run/likes.log
-py lnpybot/cli.py --env --headless --username LINKEDIN_EMAIL_ADDRESS --password LINKEDIN_PASSWORD --follow-network
